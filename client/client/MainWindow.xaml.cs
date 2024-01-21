aab590aad841e02b101c2c81a2770c933733bc29
nhAn1095 <nhan.1095.git@gmail.com>
2024-01-21 18:51:00 +0700

    first commit

---
+++ client/client/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void sendMessage(object sender, RoutedEventArgs e)
+        {
+            var hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5145/chatHub")
+                .Build();
+
+            hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Debug.Print(user + ":" + message);
+            });
+
+            await hubConnection.StartAsync();
+
+            // Gửi tin nhắn
+            await hubConnection.InvokeAsync("SendMessage", userId.Text, message.Text);
+        }
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,31 @@
+using server;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddSignalR();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+        app.MapHub<ChatHub>("/chatHub");
+        app.Run();
+    }
+}
